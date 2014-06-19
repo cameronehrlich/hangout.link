5a458f30314812e7e845cb72b101c0920939a2a2
cameronehrlich <cameronehrlich@gmail.com>
2014-06-19 15:01:25 -0700

    fucking secret token bullshit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HangoutLinkRails::Application.config.secret_token = 'c64f95b182434e92a06fd77f6c076a3e921a8a118f3dcd72cd2fde38859f22db288c0cac37c92810d7e3cc6f12f66aa1448fb01e273c332bd9ecebb2de3d04f0'
