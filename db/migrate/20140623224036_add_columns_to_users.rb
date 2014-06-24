d3d13e2580a0605d1d3cfe4f825519e48060ab57
cameronehrlich <cameronehrlich@gmail.com>
2014-06-24 16:24:43 -0700

    added subdomain validations, created initial basic api

--- db/migrate/20140623224036_add_columns_to_users.rb
+++ db/migrate/20140623224036_add_columns_to_users.rb
@@ -3,5 +3,6 @@ class AddColumnsToUsers < ActiveRecord::Migration
     add_column :users, :provider, :string
     add_column :users, :uid, :string
     add_column :users, :name, :string
+    add_column :users, :subdomain, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140623224036) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.string   "subdomain"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
